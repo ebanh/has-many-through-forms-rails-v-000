15557c6b5d8ba3513e80695bf9a50d66645b3e20
ebanh <ebanhernandez@gmail.com>
2018-09-18 14:15:35 +0900

    Test Git comment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+#comment to test git from VS Code
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
